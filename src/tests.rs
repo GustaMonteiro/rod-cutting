323893131e8107fcc00dd8d511981a8ba170012e
GustaMonteiro <gcvm6070@gmail.com>
2025-12-04 12:49:35 -0300

    Add more test cases

--- src/tests.rs
+++ src/tests.rs
@@ -6,12 +6,40 @@ struct TestCase {
 pub fn test_rod_cutting_variant(name: &str, algorithm: fn(&[u32]) -> u32) {
     let test_cases: Vec<TestCase> = vec![
         TestCase {
+            input: vec![],
+            expected: 0,
+        },
+        TestCase {
             input: vec![1, 5, 8, 9, 10, 17, 17, 20],
             expected: 22,
         },
         TestCase {
             input: vec![3, 5, 8, 9, 10, 17, 17, 20],
             expected: 24,
+        },
+        TestCase {
+            input: vec![1, 5, 8, 9],
+            expected: 10,
+        },
+        TestCase {
+            input: vec![1, 5, 8, 9, 10],
+            expected: 13,
+        },
+        TestCase {
+            input: vec![1, 1, 1, 1, 100],
+            expected: 100,
+        },
+        TestCase {
+            input: vec![10],
+            expected: 10,
+        },
+        TestCase {
+            input: vec![3, 4, 4, 5],
+            expected: 12,
+        },
+        TestCase {
+            input: vec![2, 5, 9, 9],
+            expected: 11,
         },
     ];
 
